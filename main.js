e533b50398f2999f085b133ca29789a5c8bbee39
Mridun Gupta <gmridun@gmail.com>
2024-08-22 09:55:55 +0530

    Refined Project Foundation
    Replaced the existing set of files with a new foundational set of files: index.html, style.css, and about.html to establish the core structure of the project.

---
+++ main.js
@@ -0,0 +1,44 @@
+/* displayText Logic */
+function displayText() {
+    document.getElementById("text").style.display = "block";
+}
+
+/* dismissText Logic */
+function dismissText() {
+    document.getElementById("text").style.display = "none";
+}
+
+/* displayEmail Logic */
+function displayEmail() {
+    document.getElementById("email").style.display = "block";
+}
+
+/* dismissEmail Logic */
+function dismissEmail() {
+    document.getElementById("email").style.display = "none";
+}
+
+/* displayX Logic */
+function displayX() {
+    document.getElementById("x").style.display = "block";
+}
+
+/* dismissX Logic */
+function dismissX() {
+    document.getElementById("x").style.display = "none";
+}
+
+/* displayWhatsapp Logic */
+function displayWhatsapp() {
+    document.getElementById("whatsapp").style.display = "block";
+}
+
+/* dismissWhatsapp Logic */
+function dismissWhatsapp() {
+    document.getElementById("whatsapp").style.display = "none";
+}
+
+/* dismissOverlay Logic */
+function dismissOverlay() {
+    document.getElementById("invalid-redirect").style.display = "none";
+}
